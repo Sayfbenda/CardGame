4c508a642e978f4515cdd283ff324b4b94b7792c
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-14 19:43:35 +0200

    Found solution to replace image

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let cartes = []
 let cartesenmain = []
 let condition = ["Taille", "Poid", "Espérence de vie"]
+const premierePos = document.getElementById("premiereposition")
+const deuxiemePos = document.getElementById("deuxiemeposition")
+const troisiemePos = document.getElementById("troisiemeposition")
 
 let mygale = new Object()
 mygale.nom = "La mygale"
 mygale.taille = 110
 mygale.poid = 90
 mygale.vie = 18*360
-mygale.image = '../img/mygale.jpg'
+mygale.image = '/img/mygale.jpg'
 cartes.push(mygale)
 
 let crocodile = new Object()
@@ -15,7 +18,7 @@ crocodile.nom = "Crocodile du Nil"
 crocodile.taille = 5500
 crocodile.poid = 850000
 crocodile.vie = 80*360
-crocodile.image = '../img/mygale.jpg'
+crocodile.image = '/img/mygale.jpg'
 cartes.push(crocodile)
 
 let chauvesouris = new Object()
@@ -23,7 +26,7 @@ chauvesouris.nom = "La chauve-souris"
 chauvesouris.taille = 140
 chauvesouris.poid = 70
 chauvesouris.vie = 20*360
-chauvesouris.image = '../img/mygale.jpg'
+chauvesouris.image = '/img/mygale.jpg'
 cartes.push(chauvesouris)
 
 let mouton = new Object()
@@ -31,7 +34,7 @@ mouton.nom = "Un mouton"
 mouton.taille = 2010
 mouton.poid = 15000
 mouton.vie = 25*360
-mouton.image = '../img/mygale.jpg'
+mouton.image = '/img/mygale.jpg'
 cartes.push(mouton)
 
 let vache = new Object()
@@ -39,7 +42,7 @@ vache.nom = "Une vache"
 vache.taille = 3010
 vache.poid = 200000
 vache.vie = 30*360
-vache.image = '../img/mygale.jpg'
+vache.image = '/img/mygale.jpg'
 cartes.push(vache)
 
 let poule = new Object()
@@ -47,7 +50,7 @@ poule.nom = "Une poule"
 poule.taille = 310
 poule.poid = 1200
 poule.vie = 5*360
-poule.image = '../img/mygale.jpg'
+poule.image = '/img/mygale.jpg'
 cartes.push(poule)
 
 console.log(cartes)
@@ -74,3 +77,10 @@ function genererRegle() {
     console.log("Condition : ", condition[Math.floor(Math.random()*condition.length)])
 }
 genererRegle()
+
+function test() {
+    let test = premierePos.firstElementChild.attributes[0]
+    console.log(test)
+    test.nodeValue = "/img/mygale.jpg"
+}
+test()
