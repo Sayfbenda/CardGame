81d80ccd4b5caf8d4206549f462e810ffeeba481
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-18 21:05:49 +0200

    Generer carte

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,27 @@ function genererCondition(listecondition) {
     let index = Math.floor(Math.random() * listecondition.length)
     let condition = listecondition[index]
     console.log("La condition de la partie est la suivante : ", condition)
+    generCarte(cartes, cartesenmain)
 }
 
+function generCarte(cartes, cartesenmain) {
+    for (let index = 0; index < 3; index++) {
+        let i = Math.floor(Math.random() * cartes.length)
+        let carte = cartes[i]
+        cartesenmain.push(carte)
+        cartes.splice[carte , 1]
+    }
+    for (let index = 0; index < cartesenmain.length; index++) {
+        console.log(cartesenmain[index].nom)
+        
+    }
+}
+
+function reset() {
+    cartesenmain = []
+}
 
 function jouer() {
+    reset()
     genererCondition(listecondition)
 }
