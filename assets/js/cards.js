174995f346a0d94881c1531bd0e9ae76f18fad9c
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-12-01 15:25:21 +0100

    refont design and system class

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,49 +1,45 @@
-let cartes = []
+let animaux = []
 
-let mygale = new Object()
-mygale.nom = "La mygale"
-mygale.taille = 110
-mygale.poid = 90
-mygale.vie = 18*360
-mygale.image = 'assets/img/mygale.jpg'
-cartes.push(mygale)
+class Animal {
+    constructor(nom, taille, poid, esperence_de_vie, image) {
+        this.nom = nom
+        this.taille = taille
+        this.poid = poid
+        this.esperence_de_vie = esperence_de_vie*360
+        this.image = image
+        animaux.push(this)
+    }
+}
 
-let crocodile = new Object()
-crocodile.nom = "Crocodile du Nil"
-crocodile.taille = 5500
-crocodile.poid = 850000
-crocodile.vie = 80*360
-crocodile.image = 'assets/img/NileCrocodile.jpg'
-cartes.push(crocodile)
+let mygale = new Animal("La mygale", 110, 15, 18, "assets/img/mygale.jpg"); // Note: Taille ajustée en cm
+let fourmi = new Animal("La fourmi", 0.01, 1, 1, "assets/img/fourmi.jpg");
+let abeille = new Animal("L'abeille", 0.1, 1.5, 3, "assets/img/abeille.jpg"); // Reine: ~3 ans
+let papillon = new Animal("Le monarque", 0.5, 10, 0.7, "assets/img/papillon.jpg"); // Vie en années (~9 mois)
+let colibri = new Animal("Le colibri", 4, 10, 5, "assets/img/colibri.jpg");
+let scorpion = new Animal("Le scorpion", 30, 18, 5, "assets/img/scorpion.jpg");
 
-let chauvesouris = new Object()
-chauvesouris.nom = "La chauve-souris"
-chauvesouris.taille = 140
-chauvesouris.poid = 70
-chauvesouris.vie = 20*360
-chauvesouris.image = 'assets/img/chauvesouris.jpg'
-cartes.push(chauvesouris)
+let chat = new Animal("Le chat domestique", 4500, 50, 15, "assets/img/chat.jpg");
+let chien = new Animal("Le labrador", 30000, 90, 12, "assets/img/chien.jpg");
+let loup = new Animal("Le loup gris", 40000, 150, 14, "assets/img/loup.jpg");
+let lion = new Animal("Le lion", 190000, 250, 12, "assets/img/lion.jpg");
+let elephant = new Animal("L'éléphant d'Afrique", 6000000, 330, 65, "assets/img/elephant.jpg");
+let girafe = new Animal("La girafe", 1200000, 550, 25, "assets/img/girafe.jpg");
+let hippopotame = new Animal("L'hippopotame", 1500000, 400, 45, "assets/img/hippo.jpg");
+let kangourou = new Animal("Le kangourou roux", 85000, 160, 22, "assets/img/kangourou.jpg");
+let gorille = new Animal("Le gorille", 160000, 170, 40, "assets/img/gorille.jpg");
+let ours = new Animal("L'ours polaire", 450000, 260, 25, "assets/img/ours.jpg");
 
-let mouton = new Object()
-mouton.nom = "Un mouton"
-mouton.taille = 2010
-mouton.poid = 15000
-mouton.vie = 25*360
-mouton.image = 'assets/img/mouton.jpg'
-cartes.push(mouton)
+let baleine = new Animal("La baleine bleue", 150000000, 3000, 85, "assets/img/baleine.jpg");
+let dauphin = new Animal("Le grand dauphin", 300000, 300, 45, "assets/img/dauphin.jpg");
+let requin = new Animal("Le grand requin blanc", 1100000, 450, 70, "assets/img/requin.jpg");
+let orque = new Animal("L'orque", 4500000, 700, 50, "assets/img/orque.jpg");
+let meduse = new Animal("La méduse", 2000, 40, 1, "assets/img/meduse.jpg");
 
-let vache = new Object()
-vache.nom = "Une vache"
-vache.taille = 3010
-vache.poid = 200000
-vache.vie = 30*360
-vache.image = 'assets/img/vache.jpg'
-cartes.push(vache)
+let aigle = new Animal("L'aigle royal", 5000, 80, 25, "assets/img/aigle.jpg");
+let autruche = new Animal("L'autruche", 110000, 250, 40, "assets/img/autruche.jpg");
+let manchot = new Animal("Le manchot empereur", 35000, 120, 20, "assets/img/manchot.jpg");
+let crocodile = new Animal("Le crocodile du Nil", 500000, 450, 75, "assets/img/crocodile.jpg");
+let tortue = new Animal("La tortue des Galapagos", 250000, 150, 150, "assets/img/tortue.jpg");
+let cameleon = new Animal("Le caméléon", 150, 30, 5, "assets/img/cameleon.jpg");
 
-let poule = new Object()
-poule.nom = "Une poule"
-poule.taille = 310
-poule.poid = 1200
-poule.vie = 5*360
-poule.image = 'assets/img/poule.jpg'
-cartes.push(poule)
+console.log(animaux)
